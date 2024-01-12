7685cbfa35296c20733de3651593608f9107fe4c
Noureddine Laassari <nlaassari@gmail.com>
2024-01-12 16:35:44 +0100

    refactor stop timer

--- popups/rest-started/index.js
+++ popups/rest-started/index.js
@@ -6,23 +6,10 @@ async function stopRestTing() {
   const result = await chrome.storage.local.get(["state"]);
   const state = result.state;
 
-  if (state === "REST_TIME") {
-    await stopRestTime();
-  } else {
-    await stopExtendedRestTime();
-  }
+  await chrome.storage.local.set({ [APP_STATE_KEY]: focusStates.none });
+  [focusStates.restTime, focusStates.extendedRestTime].forEach((t) =>
+    chrome.alarms.clear(t)
+  );
 
   updatePopUp("/popups/default/index.html");
-}
-
-async function stopRestTime() {
-  await chrome.storage.local.set({ [APP_STATE_KEY]: focusStates.none });
-
-  await chrome.alarms.clear(focusStates.restTime);
-}
-
-async function stopExtendedRestTime() {
-  await chrome.storage.local.set({ [APP_STATE_KEY]: focusStates.none });
-
-  await chrome.alarms.clear(focusStates.extendedRestTime);
 }
